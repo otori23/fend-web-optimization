3a23839a493b3b5523d7a1274ecfefbca4333bb3
otori23 <azeez.otori@gmail.com>
2015-07-09 11:00:23 -0700

    add minification for html, css, js; support dev and prod environments

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,25 @@
+'use strict';
+
 var gulp = require('gulp');
 var imageResize = require('gulp-image-resize');
 var imageop = require('gulp-image-optimization');
+var gutil = require('gulp-util');
+var minifyCss = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
+var minifyHtml = require('gulp-minify-html');
 
 var del = require('del');
 var runSequence = require('run-sequence');
+var minimist = require('minimist');
+
+var options = minimist(process.argv);
+var environment = options.environment || 'development';
 
 gulp.task('pizzeria', function () {
-	return gulp.src('src/views/images/pizzeria.jpg')
+	return gulp.src('src/**/pizzeria.jpg')
 				.pipe(imageResize({ width : 100, height : 75, crop : true, upscale : false}))
 				.pipe(imageop({optimizationLevel: 5, progressive: true, interlaced: true}))
-				.pipe(gulp.dest('views/images'));
+				.pipe(gulp.dest(''));
 });
 
 gulp.task('images', function(cb) {
@@ -18,8 +28,28 @@ gulp.task('images', function(cb) {
 				.pipe(gulp.dest(''));
 });
 
-gulp.task('clean', del.bind(null, ['*.html', 'img', 'views'], {dot: true}));
+gulp.task('html', function() {
+	return gulp.src('src/**/*.html')
+		.pipe(environment === 'production' ? minifyHtml() : gutil.noop())
+		.pipe(gulp.dest(''));
+});
+
+gulp.task('styles', function() {
+	return gulp.src('src/**/*.css')
+		.pipe(environment === 'production' ? minifyCss() : gutil.noop())
+		.pipe(gulp.dest(''));
+});
+
+gulp.task('scripts', function() {
+	return gulp.src('src/**/*.js')
+		.pipe(environment === 'production' ? uglify() : gutil.noop())
+		.pipe(gulp.dest(''));
+});
+
+gulp.task('clean', del.bind(null, ['*.html', 'css', 'img', 'js', 'views'], {dot: true}));
+
+gulp.task('build', ['default']);
 
 gulp.task('default', ['clean'], function(cb) {
-  runSequence(['pizzeria', 'images'], cb);
+  runSequence(['pizzeria', 'images', 'html', 'styles', 'scripts'], cb);
 });
